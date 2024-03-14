958cf56cec37910abf218497eeafd41db1316d2d
OldSinner <antylifepatia@gmail.com>
2024-03-14 10:40:17 +0100

    Changes in runner

--- runner.sh
+++ runner.sh
@@ -4,12 +4,14 @@ do
         f) file=${OPTARG};;
     esac
 done
+
+echo "DELETING $file.bin"
+rm $file.bin
+echo "$file.asm BUILDING..."
 nasm $file.asm
 mv $file $file.bin
+echo "$file.bin DONE"
 ./asmloader.exe $file.bin
-
 echo
 echo
 echo
-
-rm $file.bin
